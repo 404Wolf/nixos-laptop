b77b494a25f8d0cadf58bd8fd31cb794136ea543
Wolf Mermelstein <wolf@404wolf.com>
2025-02-04 02:48:37 -0500

    remove valfs unit

--- nixos/hardware/mounts.nix
+++ nixos/hardware/mounts.nix
@@ -1,8 +1,4 @@
 {
-  pkgs,
-  config,
-  ...
-}: {
   # Configure my Portable 1T drive
 
   # Set up the filesystem mount
@@ -24,32 +20,11 @@
     ACTION=="add", SUBSYSTEM=="block", ENV{ID_FS_UUID}=="065f9566-c638-4517-97ed-978e38b54477", TAG+="systemd", ENV{SYSTEMD_WANTS}="mnt-vault.automount"
   '';
 
-  # Set up valfs file system
+  # Set up valfs file system mount folder
   system.activationScripts = {
     createValfsMount = ''
       mkdir -p /mnt/valfs
       chown wolf:users /mnt/valfs
     '';
-  };
-  systemd.services.valfs = {
-    description = "Mount valfs";
-    wantedBy = ["multi-user.target"];
-    after = ["network.target"];
-    path = with pkgs; [valfs fuse deno coreutils];
-    serviceConfig = {
-      Type = "simple";
-      User = "wolf";
-      ExecStartPre = pkgs.writeShellScript "valfs-pre" ''
-        ${pkgs.coreutils}/bin/mkdir -p /mnt/valfs
-      '';
-      ExecStart = pkgs.writeShellScript "mount-valfs" ''
-        export PATH=${pkgs.fuse}/bin:/run/wrappers/bin/:$PATH
-        VAL_TOWN_API_KEY="$(cat ${config.sops.secrets."api-keys/valtown".path})"
-        export VAL_TOWN_API_KEY
-        ${pkgs.valfs}/bin/valfs mount /mnt/valfs --verbose
-      '';
-      Restart = "on-failure";
-      RemainAfterExit = true;
-    };
   };
 }
