760eb44fbe7ae5742c16967469571283c78002ce
Wolf Mermelstein <wolf@404wolf.com>
2025-12-29 14:59:50 -0500

    change hyprland wallpaper

--- home/users/wolf/desktop/hyprland/services.nix
+++ home/users/wolf/desktop/hyprland/services.nix
@@ -1,39 +1,29 @@
 {
   pkgs,
   config,
-  lib,
   ...
 }: {
   systemd.user = {
     services.wallpaper-refresh = {
-      Unit = {
-        Description = "Refresh wallpaper";
-      };
+      Unit.Description = "Refresh wallpaper";
       Service = {
         Type = "oneshot";
         ExecStart = pkgs.writeShellScript "wallpaper-refresh" ''
-          ${pkgs.curl}/bin/curl \
-            -L ${config.my.variables.wallpaper-fetch-url} \
-            -o ${config.my.variables.wallpaper-path}
-
+          options="${./photos}"
+          random_png=$(find "$options" -type f -name "*.png" | shuf -n 1)
+          cp -f "$random_png" ${config.my.variables.wallpaper-path}
           hyprctl hyprpaper reload ,"${config.my.variables.wallpaper-path}"
-
-          ${pkgs.imagemagick}/bin/convert ${config.my.variables.wallpaper-path} -resize 800x600 ${config.my.variables.low-resolution-wallpaper-path}
         '';
       };
     };
 
     timers.wallpaper-refresh = {
-      Unit = {
-        Description = "Timer for refreshing wallpaper";
-      };
+      Unit.Description = "Timer for refreshing wallpaper";
       Timer = {
         OnBootSec = "1min";
-        OnUnitActiveSec = "6h";
+        OnUnitActiveSec = "3h";
       };
-      Install = {
-        WantedBy = ["timers.target"];
-      };
+      Install.WantedBy = ["timers.target"];
     };
 
     services.blueman-applet = {
--- home/users/wolf/programs/zsh/applets/debugger.func.sh
+++ home/users/wolf/programs/zsh/applets/debugger.func.sh
@@ -1,3 +1,5 @@
+# shellcheck shell=bash
+
 nix build "$1" --keep-failed --impure && \
 BUILD_DIR=$(grep "keeping build directory" /dev/stderr 2>&1 | tail -1 | cut -d "'" -f 2) && \
 cd "$BUILD_DIR" && \
